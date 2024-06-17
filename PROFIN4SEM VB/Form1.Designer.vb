91a5585a978df31844fe64722473c721349a135e
JoseFabela <jfabela522@gmail.com>
2024-06-17 17:23:16 -0600

    Agregar archivos de proyecto.

---
+++ PROFIN4SEM VB/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSaveToJson = New System.Windows.Forms.Button()
+        Me.btnLoadFile = New System.Windows.Forms.Button()
+        Me.btnLoadFromDatabase = New System.Windows.Forms.Button()
+        Me.btnSaveToDatabase = New System.Windows.Forms.Button()
+        Me.btnSaveToXML = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnSaveToCSV = New System.Windows.Forms.Button()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnSaveToJson
+        '
+        Me.btnSaveToJson.Location = New System.Drawing.Point(600, 250)
+        Me.btnSaveToJson.Name = "btnSaveToJson"
+        Me.btnSaveToJson.Size = New System.Drawing.Size(121, 23)
+        Me.btnSaveToJson.TabIndex = 15
+        Me.btnSaveToJson.Text = "SaveToJSON"
+        Me.btnSaveToJson.UseVisualStyleBackColor = True
+        '
+        'btnLoadFile
+        '
+        Me.btnLoadFile.Location = New System.Drawing.Point(79, 345)
+        Me.btnLoadFile.Name = "btnLoadFile"
+        Me.btnLoadFile.Size = New System.Drawing.Size(75, 23)
+        Me.btnLoadFile.TabIndex = 14
+        Me.btnLoadFile.Text = "LoadFile"
+        Me.btnLoadFile.UseVisualStyleBackColor = True
+        '
+        'btnLoadFromDatabase
+        '
+        Me.btnLoadFromDatabase.Location = New System.Drawing.Point(362, 345)
+        Me.btnLoadFromDatabase.Name = "btnLoadFromDatabase"
+        Me.btnLoadFromDatabase.Size = New System.Drawing.Size(143, 23)
+        Me.btnLoadFromDatabase.TabIndex = 13
+        Me.btnLoadFromDatabase.Text = "LoadFromDatabase"
+        Me.btnLoadFromDatabase.UseVisualStyleBackColor = True
+        '
+        'btnSaveToDatabase
+        '
+        Me.btnSaveToDatabase.Location = New System.Drawing.Point(194, 345)
+        Me.btnSaveToDatabase.Name = "btnSaveToDatabase"
+        Me.btnSaveToDatabase.Size = New System.Drawing.Size(151, 23)
+        Me.btnSaveToDatabase.TabIndex = 12
+        Me.btnSaveToDatabase.Text = "SaveToDatabase"
+        Me.btnSaveToDatabase.UseVisualStyleBackColor = True
+        '
+        'btnSaveToXML
+        '
+        Me.btnSaveToXML.Location = New System.Drawing.Point(600, 206)
+        Me.btnSaveToXML.Name = "btnSaveToXML"
+        Me.btnSaveToXML.Size = New System.Drawing.Size(121, 23)
+        Me.btnSaveToXML.TabIndex = 11
+        Me.btnSaveToXML.Text = "SaveToXML"
+        Me.btnSaveToXML.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(600, 293)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Add Row"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'btnSaveToCSV
+        '
+        Me.btnSaveToCSV.Location = New System.Drawing.Point(600, 155)
+        Me.btnSaveToCSV.Name = "btnSaveToCSV"
+        Me.btnSaveToCSV.Size = New System.Drawing.Size(121, 23)
+        Me.btnSaveToCSV.TabIndex = 9
+        Me.btnSaveToCSV.Text = "SaveToCSV"
+        Me.btnSaveToCSV.UseVisualStyleBackColor = True
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(155, 82)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.RowHeadersWidth = 51
+        Me.DataGridView2.RowTemplate.Height = 24
+        Me.DataGridView2.Size = New System.Drawing.Size(387, 234)
+        Me.DataGridView2.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnSaveToJson)
+        Me.Controls.Add(Me.btnLoadFile)
+        Me.Controls.Add(Me.btnLoadFromDatabase)
+        Me.Controls.Add(Me.btnSaveToDatabase)
+        Me.Controls.Add(Me.btnSaveToXML)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnSaveToCSV)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSaveToJson As Button
+    Friend WithEvents btnLoadFile As Button
+    Friend WithEvents btnLoadFromDatabase As Button
+    Friend WithEvents btnSaveToDatabase As Button
+    Friend WithEvents btnSaveToXML As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents btnSaveToCSV As Button
+    Friend WithEvents DataGridView2 As DataGridView
+End Class
---
+++ PROFIN4SEM VB/Form1.vb
@@ -0,0 +1,274 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Windows.Forms
+Imports Newtonsoft.Json
+Imports System.Text
+Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports System.Xml
+Public Class Form1
+    Private sqlConnection As SqlConnection
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitializeComponent()
+        InitializeDatabaseConnection()
+    End Sub
+
+    Private Sub btnSaveToCSV_Click(sender As Object, e As EventArgs) Handles btnSaveToCSV.Click
+        Dim saveFileDialog As SaveFileDialog = New SaveFileDialog()
+        saveFileDialog.Filter = "CSV files (*.csv)|*.csv"
+        saveFileDialog.Title = "Save data to CSV"
+
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim filePath As String = saveFileDialog.FileName
+            SaveToCsv(filePath)
+        End If
+    End Sub
+
+    Private Sub btnSaveToXML_Click(sender As Object, e As EventArgs) Handles btnSaveToXML.Click
+        Dim saveFileDialog As SaveFileDialog = New SaveFileDialog()
+        saveFileDialog.Filter = "XML files (*.xml)|*.xml"
+        saveFileDialog.Title = "Save data to XML"
+
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim filePath As String = saveFileDialog.FileName
+            SaveToXml(filePath)
+        End If
+    End Sub
+
+    Private Sub btnSaveToJson_Click(sender As Object, e As EventArgs) Handles btnSaveToJson.Click
+        Dim saveFileDialog As SaveFileDialog = New SaveFileDialog()
+        saveFileDialog.Filter = "JSON files (*.json)|*.json"
+        saveFileDialog.Title = "Save data to XML"
+
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim filePath As String = saveFileDialog.FileName
+            SaveToJson(filePath)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim dataTable As DataTable = CType(DataGridView2.DataSource, DataTable)
+        Dim newRow As DataRow = dataTable.NewRow()
+
+        For i As Integer = 0 To dataTable.Columns.Count - 1
+            newRow(i) = DBNull.Value
+        Next
+
+        dataTable.Rows.Add(newRow)
+        DataGridView2.Refresh()
+    End Sub
+
+    Private Sub btnLoadFromDatabase_Click(sender As Object, e As EventArgs) Handles btnLoadFromDatabase.Click
+        LoadDataFromDatabase()
+
+    End Sub
+
+    Private Sub btnSaveToDatabase_Click(sender As Object, e As EventArgs) Handles btnSaveToDatabase.Click
+        SaveDataToDatabase()
+
+    End Sub
+
+    Private Sub btnLoadFile_Click(sender As Object, e As EventArgs) Handles btnLoadFile.Click
+        Dim openFileDialog As OpenFileDialog = New OpenFileDialog With {
+               .Filter = "CSV files (*.csv)|*.csv|XML files (*.xml)|*.xml|JSON files (*.json)|*.json"
+           }
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim filePath As String = openFileDialog.FileName
+            Dim extension As String = Path.GetExtension(filePath).ToLower()
+
+            Select Case extension
+                Case ".csv"
+                    LoadCsv(filePath)
+                Case ".xml"
+                    LoadXml(filePath)
+                Case ".json"
+                    LoadJson(filePath)
+                Case Else
+                    MessageBox.Show("Unsupported file format")
+            End Select
+        End If
+    End Sub
+
+
+    Protected Overrides Sub OnPaint(ByVal e As PaintEventArgs)
+        MyBase.OnPaint(e)
+
+        Using brush As LinearGradientBrush = New LinearGradientBrush(Me.ClientRectangle, Color.White, Color.LightBlue, 45.0F)
+            e.Graphics.FillRectangle(brush, Me.ClientRectangle)
+        End Using
+    End Sub
+
+
+
+    Private Sub InitializeDatabaseConnection()
+        Dim connectionString As String = "Server=FABELA;Database=ProyectoFinal4Sem;Integrated Security=True;"
+        sqlConnection = New SqlConnection(connectionString)
+    End Sub
+    Private Sub LoadDataFromDatabase()
+        Try
+            SqlConnection.Open()
+            Dim sqlDataAdapter As SqlDataAdapter = New SqlDataAdapter("SELECT * FROM Employee", SqlConnection)
+            Dim dataTable As DataTable = New DataTable()
+            sqlDataAdapter.Fill(dataTable)
+            DataGridView2.DataSource = dataTable
+        Catch ex As Exception
+            MessageBox.Show("Error loading data: " & ex.Message)
+        Finally
+            SqlConnection.Close()
+        End Try
+    End Sub
+    Private Sub SaveDataToDatabase()
+        Try
+            SqlConnection.Open()
+            Dim sqlDataAdapter As SqlDataAdapter = New SqlDataAdapter("SELECT * FROM Employee", SqlConnection)
+            Dim sqlCommandBuilder As SqlCommandBuilder = New SqlCommandBuilder(sqlDataAdapter)
+            Dim dataTable As DataTable = CType(DataGridView2.DataSource, DataTable)
+            sqlDataAdapter.Update(dataTable)
+        Catch ex As Exception
+            MessageBox.Show("Error saving data: " & ex.Message)
+        Finally
+            SqlConnection.Close()
+        End Try
+    End Sub
+
+    Private Sub LoadCsv(ByVal filePath As String)
+        Try
+            Dim dataTable As DataTable = New DataTable()
+
+            Using sr As StreamReader = New StreamReader(filePath)
+                Dim headers As String() = sr.ReadLine().Split(","c)
+
+                For Each header As String In headers
+                    dataTable.Columns.Add(header)
+                Next
+
+                While Not sr.EndOfStream
+                    Dim rows As String() = sr.ReadLine().Split(","c)
+                    Dim dataRow As DataRow = dataTable.NewRow()
+
+                    For i As Integer = 0 To headers.Length - 1
+                        dataRow(i) = rows(i)
+                    Next
+
+                    dataTable.Rows.Add(dataRow)
+                End While
+            End Using
+
+            DataGridView2.DataSource = dataTable
+        Catch ex As Exception
+            MessageBox.Show("Error loading CSV: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub LoadXml(ByVal filePath As String)
+        Try
+            Dim dataSet As DataSet = New DataSet()
+            dataSet.ReadXml(filePath)
+            DataGridView2.DataSource = dataSet.Tables(0)
+        Catch ex As Exception
+            MessageBox.Show("Error loading XML: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub LoadJson(ByVal filePath As String)
+        Try
+            Dim jsonData As String = File.ReadAllText(filePath)
+            Dim dataTable As DataTable = JsonConvert.DeserializeObject(Of DataTable)(jsonData)
+            DataGridView2.DataSource = dataTable
+        Catch ex As Exception
+            MessageBox.Show("Error loading JSON: " & ex.Message)
+        End Try
+    End Sub
+
+
+
+
+    Private Sub SaveToXml(ByVal filePath As String)
+        Try
+            Dim dataTable As DataTable = CType(DataGridView2.DataSource, DataTable)
+
+            If dataTable Is Nothing OrElse dataTable.Rows.Count = 0 Then
+                MessageBox.Show("No data to save!")
+                Return
+            End If
+
+            Dim dataSet As DataSet = New DataSet()
+            dataSet.Tables.Add(dataTable.Copy())
+            dataSet.WriteXml(filePath)
+            MessageBox.Show("Data saved successfully to XML: " & filePath)
+        Catch ex As Exception
+            MessageBox.Show("Error saving to XML: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub SaveToCsv(ByVal filePath As String)
+        Try
+            Dim dataTable As DataTable = CType(DataGridView2.DataSource, DataTable)
+
+            If dataTable Is Nothing OrElse dataTable.Rows.Count = 0 Then
+                MessageBox.Show("No data to save!")
+                Return
+            End If
+
+            Dim csvData As StringBuilder = New StringBuilder()
+
+            For Each column As DataColumn In dataTable.Columns
+                csvData.Append(column.ColumnName & ",")
+            Next
+
+            csvData.Remove(csvData.Length - 1, 1)
+            csvData.AppendLine()
+
+            For Each row As DataRow In dataTable.Rows
+
+                For i As Integer = 0 To row.ItemArray.Length - 1
+                    Dim cellValue As String = row.ItemArray(i).ToString()
+                    csvData.Append(cellValue & ",")
+                Next
+
+                csvData.Remove(csvData.Length - 1, 1)
+                csvData.AppendLine()
+            Next
+
+            File.WriteAllText(filePath, csvData.ToString())
+            MessageBox.Show("Data saved successfully to CSV: " & filePath)
+        Catch ex As Exception
+            MessageBox.Show("Error saving to CSV: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub SaveToJson(ByVal filePath As String)
+        Try
+            Dim dataTable As DataTable = CType(DataGridView2.DataSource, DataTable)
+
+            If dataTable Is Nothing OrElse dataTable.Rows.Count = 0 Then
+                MessageBox.Show("No data to save!")
+                Return
+            End If
+
+            Dim jsonData As List(Of Dictionary(Of String, Object)) = New List(Of Dictionary(Of String, Object))()
+
+            For Each row As DataRow In dataTable.Rows
+                Dim rowData As Dictionary(Of String, Object) = New Dictionary(Of String, Object)()
+
+                For i As Integer = 0 To row.ItemArray.Length - 1
+                    rowData.Add(dataTable.Columns(i).ColumnName, row.ItemArray(i))
+                Next
+
+                jsonData.Add(rowData)
+            Next
+
+            Dim jsonString As String = JsonConvert.SerializeObject(jsonData, Newtonsoft.Json.Formatting.Indented) ' Aquí especificamos el espacio de nombres completo
+            File.WriteAllText(filePath, jsonString)
+            MessageBox.Show("Data saved successfully to JSON: " & filePath)
+        Catch ex As Exception
+            MessageBox.Show("Error saving to JSON: " & ex.Message)
+        End Try
+    End Sub
+
+End Class
